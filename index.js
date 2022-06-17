456c1490fcacf80fdfd404c36c72fa31a259fd6a
Koima-Sam <sam17koima@gmail.com>
2022-06-17 18:38:55 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Add your code here
+// // Add your code here
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+// };
+
+// const configurationObject = {
+//     method: "POST",
+//     haeders: {
+//         'Content-Type': 'application/json',
+//         Accept: 'application/json',
+
+//     },
+//     body: JSON.stringify(formData),
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//   .then(function (response) {
+//     return response.json();
+//   })
+// .then(function (object) {
+//     console.log(object);
+// })
+// .catch(function (error) {
+//     alert("Bad things! Ragnarők!");
+//     console.log(error.message);
+// });
+
+function submitData(name, email){
+    return  fetch('http://localhost:3000/users', {
+        method : "POST",
+        headers : {
+            'Content-Type' : "application/json",
+            Accept : "application/json"
+        },
+        body : JSON.stringify({
+            name,
+            email
+        }),
+    })
+    .then(resp =>resp.json())
+    .then(function (user){
+        document.querySelector('body').innerHTML = user.id;
+    })
+    .catch((error) => document.querySelector('body').innerHTML = error.message)
+}
